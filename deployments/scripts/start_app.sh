f0c8ba80d13f3a76c7a6286d32ee642f40666df2
hang.vuthithu <thuhang23102004@gmail.com>
2025-03-15 15:53:58 +0700

    fix cicd

--- deployments/scripts/start_app.sh
+++ deployments/scripts/start_app.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sudo chmod -R 777 /home/ec2-user/caphe-backend
-~/.nvm/versions/node/v22.12.0/bin/npm install
-~/.nvm/versions/node/v22.12.0/bin/npm run build
-~/.nvm/versions/node/v22.12.0/bin/pm2 start caphe
+
+# Load nvm for the script
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm use 22
+
+# Use full paths or rely on nvm loading
+npm install
+npm run build
+pm2 start caphe
