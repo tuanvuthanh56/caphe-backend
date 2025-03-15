130e0d38d6193de4e9b23772108b89b3339aaceb
hang.vuthithu <thuhang23102004@gmail.com>
2025-03-15 16:02:55 +0700

    fix

--- deployments/scripts/stop_app.sh
+++ deployments/scripts/stop_app.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+sudo chmod -R 777 /home/ec2-user/caphe-backend
+
+# Load nvm for the script
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm use 22
+
+
 echo "Stopping any existing node servers"
 pm2 stop caphe
