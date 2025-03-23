04925bf8aebda155646cb9b92438f5b441c35c36
hang.vuthithu <thuhang23102004@gmail.com>
2025-03-23 15:38:01 +0700

    fix

--- deployments/scripts/start_app.sh
+++ deployments/scripts/start_app.sh
@@ -2,12 +2,12 @@
 sudo chmod -R 777 /home/ec2-user/caphe-backend
 sudo chown -R ec2-user:ec2-user /home/ec2-user/caphe-backend
 sudo chmod +x deployments/scripts/start_app.sh
-
+echo $(pwd)
 # Run as ec2-user
-sudo -i -u ec2-user bash << 'EOF'
-export NVM_DIR="/home/ec2-user/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm use 22
+# sudo -i -u ec2-user bash << 'EOF'
+# export NVM_DIR="/home/ec2-user/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# nvm use 22
 
 npm install
 npm run build
